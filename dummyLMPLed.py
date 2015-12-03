12668fb6a2d14d2214301acb1b14f660f622aba3
Pierre-Gilles Levallois <pgl@erasme.org>
2015-12-03 02:12:33 +0000

    suppression des dépendances avec l'interface graphique

--- dummyLMPLed.py
+++ dummyLMPLed.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import pygame
+#import pygame
 import LMPLed
            
 #################################################################
@@ -10,25 +10,25 @@ import LMPLed
 leds = LMPLed.LMPLed() # LEDS driver
 leds.setColor(leds.WHITE)
 
-pygame.init()
-fen = pygame.display.set_mode((200,200))
+#pygame.init()
+#fen = pygame.display.set_mode((200,200))
 
+leds.wait(2.0)
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYUP:
-            print("eventKey : " + str(event.key))
+#     for event in pygame.event.get():
+#         if event.type == pygame.KEYUP:
+#             print("eventKey : " + str(event.key))
             
-            if event.key == pygame.K_UP:
-                leds.fadeIn(10.0)
+#             if event.key == pygame.K_UP:
+#                 leds.fadeIn(10.0)
 
-            if event.key == pygame.K_DOWN:
-                leds.fadeOut(5.0)
+#             if event.key == pygame.K_DOWN:
+#                 leds.fadeOut(5.0)
                 
-            if event.key == pygame.K_LEFT:
-                leds.fix()
+#             if event.key == pygame.K_LEFT:
+#                 leds.fix()
                 
-            if event.key == pygame.K_RIGHT:
-                leds.wait(2.0)
+#             if event.key == pygame.K_RIGHT:
             
     # Fade to white
     leds.update()
