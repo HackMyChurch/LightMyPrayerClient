7ba582353c59076e4dd01af4f278cb0c55638b7e
Pierre-Gilles Levallois <pgl@erasme.org>
2016-10-19 15:54:08 +0200

    suppress unbuffered option for python (reduces R/W on SD)

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,7 +5,7 @@
 # Stop LightMyPrayer Client
 #
 ###############################################################################
-ps aux | grep "sudo python -u client.py" | grep -v "grep" | while read user pid z
+ps aux | grep "sudo python client.py" | grep -v "grep" | while read user pid z
 do
 	echo "Stopping LMP Client (pid=$pid)"
 	sudo kill -TERM $pid
