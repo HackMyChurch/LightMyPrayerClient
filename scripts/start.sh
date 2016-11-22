720f07bf8a3a09d45ebfa8a1b0c940f6363e63f9
Pierre-Gilles Levallois <pgl@erasme.org>
2016-11-22 23:56:52 +0100

    monitoring command

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,12 +12,12 @@ if [ "x$L1" = "xtty1" ]; then
 	clear
 	echo "   __ _       _     _                      ___                           "
 	echo "  / /(_) __ _| |__ | |_    /\/\  _   _    / _ \_ __ __ _ _   _  ___ _ __ "
-	echo " / / | |/ _\` | '_ \| __|  /    \| | | |  / /_)/ '__/ _\` | | | |/ _ \ '__|"
+	echo " / / | |/ _\` | '_ \| __| /    \| | | |  / /_)/ '__/ _\` | | | |/ _ \ '__|"
 	echo "/ /__| | (_| | | | | |_  / /\/\ \ |_| | / ___/| | | (_| | |_| |  __/ |   "
 	echo "\____/_|\__, |_| |_|\__| \/    \/\__, | \/    |_|  \__,_|\__, |\___|_|   "
 	echo "        |___/                    |___/                   |___/           "
 	cd /home/pi/lmp
-	python LMPmonitor.py &
+	sudo python LMPmonitor.py &
 	# Echoing and Saving Both Stdout And Stderr
 	# http://www.skorks.com/2009/09/using-bash-to-output-to-screen-and-file-at-the-same-time/
 	# Option -u pour être en mode 'unbuffered' et envoyer tout de suite sur la sortie standard 
